83e21232f44f9813ebbb201bfab2825f8ef4eec5
studymachine <1771134505@qq.com>
2022-07-01 17:45:37 +0800

    [A][common]: Add configuration

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "commonjs": true,
+    "es6": true,
+    "jest/globals": true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: [
+    'jest',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error'
+  }
+}
+  
